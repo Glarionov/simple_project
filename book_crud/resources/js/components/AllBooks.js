d8e326f3a151be319f5c88733de8c51360563ae2
Glarionov <artevy@mail.ru>
2020-11-15 00:12:00 +0300

    Basic web routing, 404 page styling

---
+++ book_crud/resources/js/components/AllBooks.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class AllBooks extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return  (<div className="all-books-list">
+            Here is all books
+        </div>)
+    }
+}
+
+export default AllBooks;
--- book_crud/resources/js/components/Main.js
+++ book_crud/resources/js/components/Main.js
@@ -1,16 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import NotFound from './NotFound';
+import AllBooks from './AllBooks';
+
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
 
 function Main() {
     return (
-        <div className="container">
-            Now I am reactive!
+        <div className="main-content">
+            <Router>
+                <Switch>
+                    <Route path="/all_books" component={AllBooks}/>
+                    <Route component={NotFound}/>
+                </Switch>
+            </Router>
         </div>
     );
 }
 
-export default Main;
-
 if (document.getElementById('main-wrapper')) {
-    ReactDOM.render(<Main />, document.getElementById('main-wrapper'));
+    ReactDOM.render(<Main/>, document.getElementById('main-wrapper'));
 }
